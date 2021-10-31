b075d0fe9d66fa667c2902ebc10b401d803bfdb7
Dharmeshhh <Dharmeshprajapati133@gmail.com>
2021-10-30 22:13:25 -0400

    Add project files.

---
+++ ObjectOriented/ObjectOrientedConcepts.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOriented
+{
+    public class ObjectOrientedConcepts
+    {
+        public String Name;
+        public ObjectOrientedConcepts(String Name) {
+            this.Name = Name;
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Name :" + Name);
+        }
+
+
+        // ref keyword
+        public void NameModificationsRef(ref String Name)
+        {
+            Name = "Megatron";
+        }
+
+        //out keyword
+        public void NameModificationsOut(out String Name)
+        {
+            Name = "Cybertron";
+        }
+
+        //in keyword
+        public void NameModificationsIn(in String Name)
+        {
+            Console.WriteLine(Name);
+            // We cannot change the input variable cause it is read only property
+        }
+
+        // ref keyword C# 7.0 for returning object clone or so called ref return feature
+
+        public ref String RefReturn()
+        {
+            return ref Name;
+        }
+
+        public static int Age()
+        {
+            return 24;
+        }
+    }
+}
---
+++ ObjectOriented/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ObjectOriented
+{
+   
+    public class ObjectOrientedMain 
+    {
+        public static void Main()
+        {
+            ObjectOrientedConcepts o = new ObjectOrientedConcepts("Dharmesh");
+            o.PrintName(); //op Dharmesh
+            o.NameModificationsRef(ref o.Name);
+            o.PrintName(); //op Megatron
+            o.NameModificationsOut(out o.Name /*change value of object entity from here so Megatron will be replaced by cybertron inside the function cause we are passing reference as a out argument*/);
+            o.PrintName(); //op cybertron
+            ref String refName = ref o.RefReturn();
+            refName = "Optimus Prime"; // we are changing output copy 
+            o.PrintName();
+            Console.ReadKey();
+        }
+    }
+}
